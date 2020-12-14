ac322a38d4df67069842ec8499ea16a1f580f947
Külli Kangro <kangro.kylli@gmai..com>
2020-12-14 23:54:49 +0200

    Re-add home screen content

--- app/src/main/java/com/example/shelter/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/shelter/ui/home/HomeFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.example.shelter.MainActivity
 import com.example.shelter.R
+
 
 class HomeFragment: Fragment() {
 
@@ -15,6 +18,14 @@ class HomeFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        view.findViewById<Button>(R.id.button_call).setOnClickListener {
+            (activity as MainActivity).dialPhoneNumber(resources.getString(R.string.phone_number_visit))
+        }
+
+        view.findViewById<Button>(R.id.button_call_reception).setOnClickListener {
+            (activity as MainActivity).dialPhoneNumber(resources.getString(R.string.phone_number_visit))
+        }
 
         return view
     }
